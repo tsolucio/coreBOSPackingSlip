4c355b680e94d43d6616d187014092045db14b03
Joe Bordes <joe@tsolucio.com>
2014-09-18 14:26:44 +0200

    initial version of module started fro ITS4You IssueCards updated to coreBOS

---
+++ modules/Issuecards/Save.php
@@ -0,0 +1,60 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+global $current_user, $currentModule;
+
+checkFileAccessForInclusion("modules/$currentModule/$currentModule.php");
+require_once("modules/$currentModule/$currentModule.php");
+
+$focus = new $currentModule();
+setObjectValuesFromRequest($focus);
+
+$mode = $_REQUEST['mode'];
+$record=$_REQUEST['record'];
+if($mode) $focus->mode = $mode;
+if($record)$focus->id  = $record;
+$focus->column_fields['currency_id'] = $_REQUEST['inventory_currency'];
+$cur_sym_rate = getCurrencySymbolandCRate($_REQUEST['inventory_currency']);
+$focus->column_fields['conversion_rate'] = $cur_sym_rate['rate'];
+
+if($_REQUEST['assigntype'] == 'U')  {
+	$focus->column_fields['assigned_user_id'] = $_REQUEST['assigned_user_id'];
+} elseif($_REQUEST['assigntype'] == 'T') {
+	$focus->column_fields['assigned_user_id'] = $_REQUEST['assigned_group_id'];
+}
+
+$focus->save($currentModule);
+$return_id = $focus->id;
+
+$search = vtlib_purify($_REQUEST['search_url']);
+
+$parenttab = getParentTab();
+if(isset($_REQUEST['return_module']) && $_REQUEST['return_module'] != "") {
+	$return_module = vtlib_purify($_REQUEST['return_module']);
+} else {
+	$return_module = $currentModule;
+}
+
+if(isset($_REQUEST['return_action']) && $_REQUEST['return_action'] != "") {
+	$return_action = vtlib_purify($_REQUEST['return_action']);
+} else {
+	$return_action = "DetailView";
+}
+
+if(isset($_REQUEST['return_id']) && $_REQUEST['return_id'] != "") {
+	$return_id = vtlib_purify($_REQUEST['return_id']);
+}
+
+//code added for returning back to the current view after edit from list view
+if($_REQUEST['return_viewname'] == '') $return_viewname='0';
+if($_REQUEST['return_viewname'] != '')$return_viewname=vtlib_purify($_REQUEST['return_viewname']);
+
+header("Location: index.php?action=$return_action&module=$return_module&parenttab=$parenttab&record=$return_id&viewname=$return_viewname&start=".vtlib_purify($_REQUEST['pagenumber']).$search);
+
+?>
---
+++ modules/Issuecards/language/es_es.lang.php
@@ -0,0 +1,44 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+$mod_strings = array(
+"LBL_MODULE_NAME"=>"FB",
+"LBL_MODULE_TITLE"=>"FB",
+"LBL_ISSUECARDS_INFO"=>"Información FB",
+"LBL_CUSTOM_INFORMATION"=>"Información Personalizada",
+"LBL_DESCRIPTION_INFO"=>"Descripción",
+"LBL_RELATED_PRODUCTS"=>"Productos",
+"LBL_RECALCULATE_STOCK"=>"Recalcular stock",
+
+"Issuecards No"=>"Referencia FB",
+"Description"=>"Descripción",
+"Created Time"=>"Fecha Creación",
+"Modified Time"=>"Fecha Modificación",
+"LBL_ACTION"=>"Acción",
+"Issuecards"=>"FB",
+"SINGLE_Issuecards"=>"FB",
+"Issuecard Date"=>"Fecha emisión",
+
+"LBL_RECALCULATE_QUESTION"=>"¿Seguro que quieres recalcular el stock de los productos?",
+
+'num_fb' => 'Nº FB',
+'ctoid' => 'Persona de Contacto',
+'accid' => 'Cuenta',
+'fecha_pago' => 'Fecha de Pago',
+'invoicestatus' => 'Estado',
+'calle' => 'Dirección',
+'poblacion' => 'Población',
+'provincia' => 'Provincia',
+'cpostal' => 'Código Postal',
+'pais' => 'País',
+
+);
+?>
