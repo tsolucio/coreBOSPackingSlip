1b19444710579726c31493a5c83329b900763056
Joe Bordes <joe@tsolucio.com>
2018-08-21 01:36:12 +0200

    cbSR()

--- modules/Issuecards/Issuecards.php
+++ modules/Issuecards/Issuecards.php
@@ -7,44 +7,45 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-require_once('data/CRMEntity.php');
-require_once('data/Tracker.php');
-require_once('user_privileges/default_module_view.php');
-require_once('modules/InventoryDetails/InventoryDetails.php');
+require_once 'data/CRMEntity.php';
+require_once 'data/Tracker.php';
+require_once 'user_privileges/default_module_view.php';
+require_once 'modules/InventoryDetails/InventoryDetails.php';
 
 class Issuecards extends CRMEntity {
-	var $db, $log; // Used in class functions of CRMEntity
+	public $db;
+	public $log;
 
-	var $table_name = 'vtiger_issuecards';
-	var $table_index= 'issuecardid';
-	var $column_fields = Array();
+	public $table_name = 'vtiger_issuecards';
+	public $table_index= 'issuecardid';
+	public $column_fields = array();
 
 	/** Indicator if this is a custom module or standard module */
-	var $IsCustomModule = true;
-	var $HasDirectImageField = false;
-	var $tab_name = Array('vtiger_crmentity','vtiger_issuecards','vtiger_issuecardscf');
-	var $tab_name_index = Array('vtiger_crmentity'=>'crmid','vtiger_issuecards'=>'issuecardid','vtiger_issuecardscf'=>'issuecardid');
+	public $IsCustomModule = true;
+	public $HasDirectImageField = false;
+	public $tab_name = array('vtiger_crmentity','vtiger_issuecards','vtiger_issuecardscf');
+	public $tab_name_index = array('vtiger_crmentity'=>'crmid','vtiger_issuecards'=>'issuecardid','vtiger_issuecardscf'=>'issuecardid');
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
-	var $customFieldTable = Array('vtiger_issuecardscf', 'issuecardid');
-	var $entity_table = 'vtiger_crmentity';
+	public $customFieldTable = array('vtiger_issuecardscf', 'issuecardid');
+	public $entity_table = 'vtiger_crmentity';
 
-	var $object_name = 'Issuecards';
+	public $object_name = 'Issuecards';
 
-	var $update_product_array = Array();
+	public $update_product_array = array();
 
 	// This is the list of vtiger_fields that are in the lists.
-	var $list_fields = Array (
-		'Issuecards No'=> Array('project' => 'issuecards_no'),
-		'ctoid' => Array('issuecards' => 'ctoid'),
-		'accid' => Array('issuecards' => 'accid'),
-		'fecha_pago' => Array('issuecards' => 'fecha_pago'),
-		'invoicestatus' => Array('issuecards' => 'invoicestatus'),
-		'Total'=>Array('issuecards'=>'hdnGrandTotal'),
-		'Assigned To' => Array('crmentity' => 'smownerid')
+	public $list_fields = array(
+		'Issuecards No'=> array('project' => 'issuecards_no'),
+		'ctoid' => array('issuecards' => 'ctoid'),
+		'accid' => array('issuecards' => 'accid'),
+		'fecha_pago' => array('issuecards' => 'fecha_pago'),
+		'invoicestatus' => array('issuecards' => 'invoicestatus'),
+		'Total'=>array('issuecards'=>'hdnGrandTotal'),
+		'Assigned To' => array('crmentity' => 'smownerid')
 	);
-	var $list_fields_name = Array(
+	public $list_fields_name = array(
 		'Issuecards No'=> 'issuecards_no',
 		'ctoid' => 'ctoid',
 		'accid' => 'accid',
@@ -55,17 +56,17 @@ class Issuecards extends CRMEntity {
 	);
 
 	// Make the field link to detail view from list view (Fieldname)
-	var $list_link_field = 'issuecards_no';
+	public $list_link_field = 'issuecards_no';
 
 	// For Popup listview and UI type support
-	var $search_fields = Array(
-		'Issuecards No'=> Array('issuecards' => 'issuecards_no'),
-		'ctoid' => Array('issuecards' => 'ctoid'),
-		'accid' => Array('issuecards' => 'accid'),
-		'fecha_pago' => Array('issuecards' => 'fecha_pago'),
-		'invoicestatus' => Array('issuecards' => 'invoicestatus'),
+	public $search_fields = array(
+		'Issuecards No'=> array('issuecards' => 'issuecards_no'),
+		'ctoid' => array('issuecards' => 'ctoid'),
+		'accid' => array('issuecards' => 'accid'),
+		'fecha_pago' => array('issuecards' => 'fecha_pago'),
+		'invoicestatus' => array('issuecards' => 'invoicestatus'),
 	);
-	var $search_fields_name = Array(
+	public $search_fields_name = array(
 		'Issuecards No'=> 'issuecards_no',
 		'ctoid' => 'ctoid',
 		'accid' => 'accid',
@@ -74,32 +75,33 @@ class Issuecards extends CRMEntity {
 	);
 
 	// For Popup window record selection
-	var $popup_fields = Array('issuecards_no', 'startdate');
+	public $popup_fields = array('issuecards_no', 'startdate');
 
-	var $sortby_fields = Array();
+	// Placeholder for sort fields - All the fields will be initialized for Sorting through initSortFields
+	public $sortby_fields = array();
 
 	// For Alphabetical search
-	var $def_basicsearch_col = 'issuecards_no';
+	public $def_basicsearch_col = 'issuecards_no';
 
 	// Column value to use on detail view record text display
-	var $def_detailview_recname = 'issuecards_no';
+	public $def_detailview_recname = 'issuecards_no';
 
 	// Required Information for enabling Import feature
-	var $required_fields = Array('issuecards_no'=>1);
+	public $required_fields = array('issuecards_no'=>1);
 
 	// Callback function list during Importing
-	var $special_functions = Array('set_import_assigned_user');
+	public $special_functions = array('set_import_assigned_user');
 
-	var $default_order_by = 'issuecards_no';
-	var $default_sort_order='ASC';
+	public $default_order_by = 'issuecards_no';
+	public $default_sort_order='ASC';
 	// Used when enabling/disabling the mandatory fields for the module.
 	// Refers to vtiger_field.fieldname values.
-	var $mandatory_fields = Array('createdtime', 'modifiedtime', 'issuecards_no');
+	public $mandatory_fields = array('createdtime', 'modifiedtime', 'issuecards_no');
 
-	function save_module($module) {
+	public function save_module($module) {
 		global $updateInventoryProductRel_deduct_stock;
 		if ($this->HasDirectImageField) {
-			$this->insertIntoAttachment($this->id,$module);
+			$this->insertIntoAttachment($this->id, $module);
 		}
 		$updateInventoryProductRel_deduct_stock = true;
 
@@ -119,7 +121,7 @@ class Issuecards extends CRMEntity {
 		$this->db->pquery($update_query, $update_params);
 	}
 
-	function restore($module, $id) {
+	public function restore($module, $id) {
 		global $current_user;
 		$this->db->println("TRANS restore starts $module");
 		$this->db->startTransaction();		
@@ -142,26 +144,26 @@ class Issuecards extends CRMEntity {
 
 	/*Function to create records in current module.
 	**This function called while importing records to this module*/
-	function createRecords($obj) {
+	public function createRecords($obj) {
 		$createRecords = createRecords($obj);
 		return $createRecords;
 	}
 
 	/*Function returns the record information which means whether the record is imported or not
 	**This function called while importing records to this module*/
-	function importRecord($obj, $inventoryFieldData, $lineItemDetails) {
+	public function importRecord($obj, $inventoryFieldData, $lineItemDetails) {
 		$entityInfo = importRecord($obj, $inventoryFieldData, $lineItemDetails);
 		return $entityInfo;
 	}
 
 	/*Function to return the status count of imported records in current module.
 	**This function called while importing records to this module*/
-	function getImportStatusCount($obj) {
+	public function getImportStatusCount($obj) {
 		$statusCount = getImportStatusCount($obj);
 		return $statusCount;
 	}
 
-	function undoLastImport($obj, $user) {
+	public function undoLastImport($obj, $user) {
 		$undoLastImport = undoLastImport($obj, $user);
 	}
 
@@ -169,7 +171,7 @@ class Issuecards extends CRMEntity {
 	* @param reference variable - where condition is passed when the query is executed
 	* Returns Export Issuecards Query.
 	*/
-	function create_export_query($where) {
+	public function create_export_query($where) {
 		global $log, $current_user;
 		$log->debug("Entering create_export_query(".$where.") method ...");
 
@@ -211,22 +213,22 @@ class Issuecards extends CRMEntity {
 	 * @param String Module name
 	 * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
 	 */
-	function vtlib_handler($modulename, $event_type) {
+	public function vtlib_handler($modulename, $event_type) {
 		global $adb;
-		require_once('include/events/include.inc');
-		include_once('vtlib/Vtiger/Module.php');
-		if($event_type == 'module.postinstall') {
+		require_once 'include/events/include.inc';
+		include_once 'vtlib/Vtiger/Module.php';
+		if ($event_type == 'module.postinstall') {
 			// TODO Handle post installation actions
 			$modAccounts=Vtiger_Module::getInstance('Accounts');
 			$modContacts=Vtiger_Module::getInstance('Contacts');
 			$modInvD=Vtiger_Module::getInstance('InventoryDetails');
 			$modIss=Vtiger_Module::getInstance('Issuecards');
-			if ($modAccounts) $modAccounts->setRelatedList($modIss, 'Issuecards', Array('ADD'),'get_dependents_list');
-			if ($modContacts) $modContacts->setRelatedList($modIss, 'Issuecards', Array('ADD'),'get_dependents_list');
+			if ($modAccounts) $modAccounts->setRelatedList($modIss, 'Issuecards', array('ADD'),'get_dependents_list');
+			if ($modContacts) $modContacts->setRelatedList($modIss, 'Issuecards', array('ADD'),'get_dependents_list');
 			if ($modInvD){
 				$field = Vtiger_Field::getInstance('related_to',$modInvD);
 				$field->setRelatedModules(array('Issuecards'));
-				$modIss->setRelatedList($modInvD, 'InventoryDetails', Array(''),'get_dependents_list');
+				$modIss->setRelatedList($modInvD, 'InventoryDetails', array(''),'get_dependents_list');
 			}
 			//Add Gendoc to Issuecards
 			if(vtlib_isModuleActive("evvtgendoc")){
@@ -253,15 +255,15 @@ class Issuecards extends CRMEntity {
 			$tm->saveTask($task);
 
 			$this->setModuleSeqNumber('configure', $modulename, 'pslip-', '0000001');
-		} else if($event_type == 'module.disabled') {
+		} elseif ($event_type == 'module.disabled') {
 			// TODO Handle actions when this module is disabled.
-		} else if($event_type == 'module.enabled') {
+		} elseif ($event_type == 'module.enabled') {
 			// TODO Handle actions when this module is enabled.
-		} else if($event_type == 'module.preuninstall') {
+		} elseif ($event_type == 'module.preuninstall') {
 			// TODO Handle actions when this module is about to be deleted.
-		} else if($event_type == 'module.preupdate') {
+		} elseif ($event_type == 'module.preupdate') {
 			// TODO Handle actions before this module is updated.
-		} else if($event_type == 'module.postupdate') {
+		} elseif ($event_type == 'module.postupdate') {
 			// TODO Handle actions after this module is updated.
 			$modInvD=Vtiger_Module::getInstance('InventoryDetails');
 			$modIss=Vtiger_Module::getInstance('Issuecards');
@@ -285,7 +287,7 @@ class Issuecards extends CRMEntity {
 			if ($modInvD){
 				$field = Vtiger_Field::getInstance('related_to',$modInvD);
 				$field->setRelatedModules(array('Issuecards'));
-				$modIss->setRelatedList($modInvD, 'InventoryDetails', Array(''),'get_dependents_list');
+				$modIss->setRelatedList($modInvD, 'InventoryDetails', array(''),'get_dependents_list');
 			}
 			//Add Gendoc to Issuecards
 			if(vtlib_isModuleActive("evvtgendoc")){
@@ -317,8 +319,7 @@ class Issuecards extends CRMEntity {
 	* @param  integer   $id
 	* returns related Task or Event record in array format
 	*/
-	function get_history($id, $cur_tab_id, $rel_tab_id, $actions)
-	{
+	public function get_history($id, $cur_tab_id, $rel_tab_id, $actions) {
 			global $log;
 			$log->debug("Entering get_history(".$id.") method ...");
 			$query = "SELECT vtiger_activity.activityid, vtiger_activity.subject, vtiger_activity.status,
@@ -346,10 +347,10 @@ class Issuecards extends CRMEntity {
 	 * @param - $secmodule secondary module name
 	 * returns the query string formed on fetching the related data for report for secondary module
 	 */
-	function generateReportsSecQuery($module,$secmodule,$type = '',$where_condition = ''){
-		$query = $this->getRelationQuery($module,$secmodule,"vtiger_issuecards","issuecardid");
+	public function generateReportsSecQuery($module, $secmodule, $queryPlanner, $type = '', $where_condition = '') {
+		$query = $this->getRelationQuery($module, $secmodule, "vtiger_issuecards", "issuecardid");
 		$query .= " left join vtiger_currency_info as vtiger_currency_info$secmodule on vtiger_currency_info$secmodule.id = vtiger_issuecards.currency_id ";
-		if(($type !== 'COLUMNSTOTOTAL') || ($type == 'COLUMNSTOTOTAL' && $where_condition == 'add')) {
+		if (($type !== 'COLUMNSTOTOTAL') || ($type == 'COLUMNSTOTOTAL' && $where_condition == 'add')) {
 			$query .= "left join vtiger_inventoryproductrel as vtiger_inventoryproductrelIssuecards on vtiger_issuecards.issuecardid = vtiger_inventoryproductrelIssuecards.id
 				left join vtiger_products as vtiger_productsIssuecards on vtiger_productsIssuecards.productid = vtiger_inventoryproductrelIssuecards.productid
 				left join vtiger_service as vtiger_serviceIssuecards on vtiger_serviceIssuecards.serviceid = vtiger_inventoryproductrelIssuecards.productid ";
